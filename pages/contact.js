7945cfb54d3c47e4e36b86b19aef463436be2092
Tolkinxon <krosstony639@gmail.com>
2023-02-09 23:35:08 +0500

    meta tags changed

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+
+
+export default function blog() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>this is contact page</p>
+        </div>
+    )
+}
---
+++ pages/test/post.js
@@ -0,0 +1,8 @@
+export default function blog() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>this is contact pe</p>
+    </div>
+  )
+}
