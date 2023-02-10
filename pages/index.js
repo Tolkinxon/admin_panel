052a9d7e13d810fea8b5e213c806fcaaf04adef4
Tolkinxon <krosstony639@gmail.com>
2023-02-10 23:26:21 +0500

    added server side props

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
 
-export default function Home({ user }) {
-  // const [data, setData] = useState([])
+export default function Home() {
+  const [data, setData] = useState([])
   const [loading, setLoading] = useState(true)
 
-
-
-  // useEffect(() => {
-
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response => response.json())
-  //     .then(data => { setData(data);  setLoading(false) })
-  // }, [])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
 
   return (
     <>
@@ -25,32 +25,30 @@ export default function Home({ user }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {/* {loading ? (
+        {loading ? (
           <>
             <p>Loading...</p>
           </>
         ) : (
-          <> */}
-            {user.map((data) => (
+          <>
+            {data.map((data) => (
               <>
                 <h5 key={data.id}>{data.name}</h5>
               </>
             ))}
-          {/* </>
-        )} */}
+          </>
+        )}
       </main>
     </>
   )
 }
 
-export async function getServerSideProps() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-  const data = await response.json()
-
-  return {
-    props: {
-      user: data,
-    },
-  }
-}
+// export async function getServerSideProps() {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//   const data = await response.json()
+//   return {
+//     props: {
+//       user: data,
+//     },
+//   }
+// }
