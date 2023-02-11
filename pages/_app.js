14102539152ce881cb9698d38e1569745b284a32
Tolkinxon <krosstony639@gmail.com>
2023-02-11 21:59:13 +0500

    add seo package

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { NextSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <NextSeo 
+      titleTemplate='hello world'
+    
+    />
+    <Component {...pageProps} />
+    </>
+  )
 }
--- pages/test.js
+++ pages/test.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Head from 'next/head'
+// import Head from 'next/head'
 
 const test = () => {
   return (
     <>
-      <Head>
-        <title>ggggggg</title>
-      </Head>
+      {/* <Head>
+        <title>ggggggg</title> */}
+      {/* </Head> */}
       <div>
         <h3>hello world</h3>
       </div>
