8d4a58481810204a8a64c51dac92cca058e24db2
Tolkinxon <krosstony639@gmail.com>
2023-02-11 21:53:55 +0500

    adde some title tags

---
+++ pages/test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+const test = () => {
+  return (
+    <>
+      <Head>
+        <title>ggggggg</title>
+      </Head>
+      <div>
+        <h3>hello world</h3>
+      </div>
+    </>
+  )
+}
+
+export default test
---
+++ pages/test2.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+const test = () => {
+  return (
+    <>
+      <Head>
+        <title>fffff </title>
+      </Head>
+      <div>
+        <h3>hello world2</h3>
+      </div>
+    </>
+  )
+}
+
+export default test
