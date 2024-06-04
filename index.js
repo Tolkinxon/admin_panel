ed1cecbbaf59063f72a048c3d43478a2a18ffc56
Tolkinxon <krosstony639@gmail.com>
2024-06-04 14:51:40 +0300

    some changes

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-const box2 = document.querySelector('.box2')
+const box = document.querySelector('.box')
 const child = document.querySelector('.first')
 const li = document.querySelectorAll('li')
 
-let i = 0
-let j = 0
+// let i = 0
+// let j = 0
 
-let prevValueOfScroll = 0
+// let prevValueOfScroll = 0
 
-box2.addEventListener('scroll', (evt) => {
-    console.log(box2.scrollTop);
+// box2.addEventListener('scroll', (evt) => {
 
-    if(prevValueOfScroll >= box2.scrollTop) {
-        prevValueOfScroll = box2.scrollTop
-        i += 0.1
-    }
-    else {
-        prevValueOfScroll = box2.scrollTop
-        i -= 0.1
-    }
+//     console.log(box2.scrollTop);
+
+//     if(prevValueOfScroll >= box2.scrollTop) {
+//         prevValueOfScroll = box2.scrollTop
+//         i += 0.1
+//     }
+//     else {
+//         prevValueOfScroll = box2.scrollTop
+//         i -= 0.1
+//     }
+
     
 
 
-    j += i
-    li.forEach((item) => {
-        item.style.top = j * 20 + 'px'
-        j++
+//     j = i
+//     li.forEach((item) => {
+//         item.style.top = j * 20 + 'px'
+//         j++
 
-        // console.log(j);
-    })
-    j = 0
+//         // console.log(j);
+//     })
+//     j = 0
+// })
+
+
+// li.forEach((item) => {
+//     item.style.top = j * 20 + 'px'
+//     j++
+
+// })
+
+
+box.addEventListener('scroll', () => {
+    // console.log(box.scrollTop);
+
+    li.forEach(item => item.style.backgroundColor = 'white')
+
+
+    li[Math.ceil(box.scrollTop / 20)  + 1].style.backgroundColor = 'red'
+
+    // console.log(Math.ceil(box.scrollTop / 20)  + 1);
+
+
 })
-
-
-li.forEach((item) => {
-    item.style.top = j * 20 + 'px'
-    j++
-
-})
-
