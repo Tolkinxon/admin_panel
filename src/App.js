9bda2ef639f57ad29fabdce6b534e3beacf051b1
Tolkinxon <krosstony639@gmail.com>
2024-07-27 13:09:26 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Project from './components/project/Project'
 import Career from './components/career/Career'
+import './app.css'
 
 function App() {
   return (
     <div className="App">
-      <Project />
-      <Career />
+      <section className='left-side'>
+        <h2 className='left-side__h2 h2-active'>Project</h2>
+        <h2 className='left-side__h2 '>Career</h2>
+      </section>
+
+      <section className='right-side'>
+        <Project />
+        <Career />
+      </section>
     </div>
   );
 }
