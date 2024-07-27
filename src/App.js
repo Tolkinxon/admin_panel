dc10020efb6a52d59cf24a938a783d2b818e6dd4
Tolkinxon <krosstony639@gmail.com>
2024-07-27 13:57:04 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import Project from './components/project/Project'
 import Career from './components/career/Career'
 import './app.css'
+import { useState } from 'react';
 
 function App() {
+  const [isActive, setIsActive] = useState('Project')
+
+
+
   return (
     <div className="App">
       <section className='left-side'>
-        <h2 className='left-side__h2 h2-active'>Project</h2>
-        <h2 className='left-side__h2 '>Career</h2>
+        <h2 className={`left-side__h2 ${isActive == 'Project'?'h2-active':''}`} onClick={() => setIsActive('Project')}>Project</h2>
+        <h2 className={`left-side__h2 ${isActive == 'Career'?'h2-active':''}`} onClick={() => setIsActive('Career')}>Career</h2>
       </section>
 
       <section className='right-side'>
-        <Project />
-        <Career />
+       {isActive == 'Project' ? <Project /> : <></>} 
+       {isActive == 'Career' ?  <Career /> : <></>} 
       </section>
     </div>
   );
