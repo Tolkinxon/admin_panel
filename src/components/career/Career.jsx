41130c8231e48e31736497298762d96e619adaf1
Tolkinxon <krosstony639@gmail.com>
2024-08-01 14:48:11 +0300

    some changes

--- src/components/career/Career.jsx
+++ src/components/career/Career.jsx
@@ -1,24 +1,30 @@
 import './career.css'
 import { useEffect, useState } from 'react';
-import uuid from 'react-uuid';
 
 const Career = () => {
 
     const [data, setData] = useState([]);
+    const [educationData, setEducationData] = useState([]);
+    const [experienceData, setExperienceData] = useState([]);
     const [isActive, setIsActive] = useState('add');
+    const [isActiveCategory, setIsActiveCategory] = useState('experience');
     const [addPositionTasksInputs, setAddPositionTasksInputs] = useState(0)
 
 
 useEffect(() => {
     (async() => {
-        const response = await fetch('https://test.itpoint.uz/api/career/?type=experience');
-        const data = await response.json();
+        const experienceResponse = await fetch('https://test.itpoint.uz/api/career/?type=experience');
+        const dataExperience = await experienceResponse.json();
+        setExperienceData(dataExperience)
 
-        setData(data);
+        const educationResponse = await fetch('https://test.itpoint.uz/api/career/?type=education');
+        const dataEducation = await educationResponse.json();
+        setEducationData(dataEducation)
+
+        setData(dataExperience);
+
     })();
 }, []);
-
-
 
 
 
@@ -38,119 +44,7 @@ useEffect(() => {
         )
     });
 
-    const newFormElements = [1].map(item => {
-        return (
-            <>
-                <div className='inputs__wrapper' >
-                            <label htmlFor="first">
-                                Subtitle: 
-                                <input type="text" id='first' name='sub_title'/> 
-                            </label>
-
-                            <label htmlFor="second">
-                                Text: 
-                                <input type="text" id="second" name='text'/> 
-                            </label>
-
-                            <label htmlFor="third">
-                                Title: 
-                                <input type="text" id='third' name='title'/> 
-                            </label>
-
-                            <label htmlFor="fourth">
-                                Year range: 
-                                <input type="text" id="fourth" name='year_range'/> 
-                            </label>
-
-                            <label htmlFor="fourth">
-                                Year range: 
-                                <select name="type" >
-                                    <option value="experience" defaultValue={'experience'}>experience</option>
-                                    <option value="education">education</option>
-                                </select>
-                            </label>
-                </div>
-
-                <div className='position_tasks_input__wrapper'>
-                            <button className='tasks-input-btn' onClick={() => setAddPositionTasksInputs(prev => prev + 1)}>add</button>
-                            <div className='position_tasks_input__items'>
-                                <label htmlFor="fifth">
-                                    career: 
-                                    <input type="text" id="fifth" name='another_career'/> 
-                                </label>
-
-                                <label htmlFor="sixth">
-                                    text: 
-                                    <input type="text" id="sixth" name='another_text'/> 
-                                </label>
-                            </div>
-
-                            {
-                                elements
-                            }
-                </div>
-                <button type='submit'>send</button>
-            </>
-        )
-    })
-
-    const valueInner = <>
-                 <div className='inputs__wrapper' >
-                            <label htmlFor="first">
-                                Subtitle: 
-                                <input type="text" id='first' name='sub_title'/> 
-                            </label>
-
-                            <label htmlFor="second">
-                                Text: 
-                                <input type="text" id="second" name='text'/> 
-                            </label>
-
-                            <label htmlFor="third">
-                                Title: 
-                                <input type="text" id='third' name='title'/> 
-                            </label>
-
-                            <label htmlFor="fourth">
-                                Year range: 
-                                <input type="text" id="fourth" name='year_range'/> 
-                            </label>
-
-                            <label htmlFor="fourth">
-                                Year range: 
-                                <select name="type" >
-                                    <option value="experience" defaultValue={'experience'}>experience</option>
-                                    <option value="education">education</option>
-                                </select>
-                            </label>
-                </div>
-
-                <div className='position_tasks_input__wrapper'>
-                            <button className='tasks-input-btn' onClick={() => setAddPositionTasksInputs(prev => prev + 1)}>add</button>
-                            <div className='position_tasks_input__items'>
-                                <label htmlFor="fifth">
-                                    career: 
-                                    <input type="text" id="fifth" name='another_career'/> 
-                                </label>
-
-                                <label htmlFor="sixth">
-                                    text: 
-                                    <input type="text" id="sixth" name='another_text'/> 
-                                </label>
-                            </div>
-
-                            {
-                                elements
-                            }
-                </div>
-                <button type='submit'>send</button>
-    
-    </>
-
-
-
-
-    
+   
     const handleSubmit = (event) => {
 
         event.preventDefault(); 
@@ -168,7 +62,6 @@ useEffect(() => {
                 newAnotherObj[keyName] = value;
                 if(even % 2 == 0){
                     newArr[(even / 2)][keyName]=value;
-                    newArr[(even / 2)].id= (even/2);
                 }
                 else {
                     newArr.push(newAnotherObj);
@@ -180,11 +73,6 @@ useEffect(() => {
         }
         newArr.shift();
         newObj["position_tasks"] = newArr;
-        newObj.id = uuid();
-
-        console.log(newObj);
-
-
     };
 
     
@@ -192,6 +80,12 @@ useEffect(() => {
         return ( 
          <>
             <section className='career' style={{display: isActive == 'add' ? 'block' : 'none' }}>
+
+            <button className={`category-button ${isActiveCategory == 'experience' ? 'category-active':''}`} onClick={() => {setData(experienceData); setIsActiveCategory('experience');}} >experience</button>
+
+            <button className={`category-button ${isActiveCategory == 'education' ? 'category-active':''}`} onClick={() => {setData(educationData); setIsActiveCategory('education')}}>education</button>
+                    
+                
                 <div className='container'>
                 <button className='career__button' onClick={() => setIsActive('back')}>add</button>
                     {
