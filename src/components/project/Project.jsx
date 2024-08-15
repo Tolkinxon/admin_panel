05029fb3f7e94fc291b4632827b3607fca88404f
Tolkinxon <krosstony639@gmail.com>
2024-08-15 21:29:26 +0300

    some changes

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -129,11 +129,6 @@ const Project = () => {
     }
 
 
-    
-    
- 
-    
-
         return ( 
          <>
             <section className='career' style={{display: isActive == 'add' ? 'block' : 'none' }}>
@@ -148,7 +143,10 @@ const Project = () => {
                     
                 
                 <div className='container'>
-                <button className='career__button' onClick={() => setIsActive('back')}>add</button>
+                <button className='career__button' onClick={() => setIsActive('back')}>
+                <svg width={15} height={15} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill='white' d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg> 
+                    add
+                    </button>
                     {
                         data.map((item, idx) => {
                             const { cropped_photo, title, location, software, client, url, project_type, source_type, description, id} = item;
