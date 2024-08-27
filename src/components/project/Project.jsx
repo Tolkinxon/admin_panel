f36c95bbe8e92cd6575be7ef2f85669ceb80a7e3
Tolkinxon <krosstony639@gmail.com>
2024-08-27 04:09:28 +0300

    loader settings finished

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -22,9 +22,9 @@ const Project = () => {
     const [elementsData, setElementsData] = useState(0)
     const [arr, setArr] = useState([])
     const [mainPhoto, setMainPhoto] = useState('')
-
     const [type, setType] = useState('')
     const [hideMainImg, setHideMainImg] = useState(true);
+    const [loadingTitme, setLoadingTime] = useState(false)
 
     
     useEffect(() => {
@@ -86,7 +86,8 @@ const Project = () => {
             formData.delete('project_type')
             formData.delete('source_type')
             formData.delete('description')
-            
+
+                setLoadingTime(true);            
                 fetch(`https://test.itpoint.uz/api/project/${findId}/`, {
                     headers: {"Content-type": "application/json"},
                     method: "PATCH",
@@ -94,6 +95,7 @@ const Project = () => {
                 })
                 .then(response => response.json()) 
                 .then(data => {
+                    setLoadingTime(true);
                     if(!hideMainImg){
                         let newFormData = new FormData();
         
@@ -105,7 +107,7 @@ const Project = () => {
                         // for(let [key, value] of newFormData.entries()){
                         //     console.log(key, value);
                         // }
-        
+                        setLoadingTime(true);
                         fetch('https://test.itpoint.uz/api/photo/', {
                             method: "POST",
                             body: newFormData
@@ -115,6 +117,7 @@ const Project = () => {
                         .catch(console.error);
                     }
                     else {
+                        setLoadingTime(true);
                         setTimeout(() => {
                             window.location.reload()
                         }, 2500); 
@@ -122,7 +125,8 @@ const Project = () => {
 
                     if(formData.get('photo')){
                         formData.append('type', 'secondary')
-        
+
+                        setLoadingTime(true);
                         fetch('https://test.itpoint.uz/api/photo/', {
                             method: "POST",
                             body: formData
@@ -132,6 +136,7 @@ const Project = () => {
                         .catch(console.error);
                     }
                     else {
+                        setLoadingTime(true);
                         setTimeout(() => {
                             window.location.reload()
                         }, 2500);  
@@ -141,6 +146,8 @@ const Project = () => {
         }
 
         else {
+
+            setLoadingTime(true);
             fetch(endpoint, {
                 method: "POST",
                 body: formData
@@ -355,7 +362,7 @@ const Project = () => {
                             </div>
 
                             <div class="form-floating">
-                                <input type="text" name='title' class="form-control"  id="floatingPassword" placeholder="Title" />
+                                <input type="text" name='title' class="form-control"  id="floatingPassword" placeholder="Title"  required/>
                                 <label for="floatingPassword">Title</label>
                             </div> 
 
@@ -387,7 +394,8 @@ const Project = () => {
                         </div>
 
                         <button type='submit' className='send-button'>
-                        <svg width={15} height={15} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill='#fff' d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376l0 103.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>
+                        <svg width={20} height={20}  style={{display: loadingTitme ? 'none' : 'block'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill='#fff' d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376l0 103.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>
+                        <div className='loader' style={{display: loadingTitme ? 'block' : 'none'}}></div>
                             send
                             </button>
                     </form>
